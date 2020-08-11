f369b6057893a7ea14d7edf04b0e59a79064a844
TaitaT <taita.omori@gmail.com>
2020-08-11 19:21:36 +0900

    正規表現の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  validates_format_of :last_name, with: NAME_REGEX
+  validates_format_of :first_name, with: NAME_REGEX
+
+  NAME_KANA_REGEX = /\A[ァ-ン]+\z/.freeze
+  validates_format_of :last_name_kana, with: NAME_KANA_REGEX
+  validates_format_of :first_name_kana, with: NAME_KANA_REGEX
+
+  EMAIL_REGEX = /.+@.+/.freeze
+  validates_format_of :email, with: EMAIL_REGEX
+
 end
