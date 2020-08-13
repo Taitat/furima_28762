9a0c6164d0ea6fb1ce776b067efc9469e5b656fe
TaitaT <taita.omori@gmail.com>
2020-08-13 15:36:20 +0900

    単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname,:first_name,:last_name,
+            :first_name_kana,:last_name_kana,:birth_date, presence: true
+  
+
+  
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
