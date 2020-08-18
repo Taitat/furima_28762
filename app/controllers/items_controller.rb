c65c5460bc9bb4404c9d25cfde3f51ad8a24ace5
TaitaT <taita.omori@gmail.com>
2020-08-18 17:15:34 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,33 +10,27 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    binding.pry
     if @item.save
       redirect_to root_path
     else
       render :new
-      
-    end  
 
+    end
   end
 
   def account
     item_price = params[:price]
     add_tax_price = (item_price.to_i * 0.1).floor
     profit = (item_price.to_i * 0.9).floor
-    render json: {add_tax_price: add_tax_price, profit: profit}
-    
+    render json: { add_tax_price: add_tax_price, profit: profit }
   end
-
 
   private
+
   def item_params
     params.require(:item).permit(
-      :name,:image,:description,:categories_id,:conditions_id,:postage_payers_id,
-      :prefecture_id,:handling_times_id, :price
+      :name, :image, :description, :categories_id, :conditions_id, :postage_payers_id,
+      :prefecture_id, :handling_times_id, :price
     ).merge(user_id: current_user.id)
   end
-
-
-
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,23 +1,19 @@
 class Item < ApplicationRecord
-
-    belongs_to :user
-    has_one_attached :image
-    has_one :purchase
+  belongs_to :user
+  has_one_attached :image
+  has_one :purchase
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to_active_hash :category
-    belongs_to_active_hash :condition
-    belongs_to_active_hash :handling_time
-    belongs_to_active_hash :postage_payer
-    belongs_to_active_hash :prefecture
-  
-validates :image, :user, :name, :description, presence: true
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :handling_time
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :prefecture
 
-validates :price, presence: true, numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
+  validates :image, :user, :name, :description, presence: true
 
-validates :categories_id, :conditions_id, :handling_times_id,
-          :postage_payers_id, :prefecture_id, presence: true, numericality: { other_than: 1}
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
-
-
+  validates :categories_id, :conditions_id, :handling_times_id,
+            :postage_payers_id, :prefecture_id, presence: true, numericality: { other_than: 1 }
 end
