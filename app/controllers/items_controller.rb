1b34cbf4dd9a2045edb80bb1a97067cd4f491be5
TaitaT <taita.omori@gmail.com>
2020-08-19 18:16:41 +0900

    詳細ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,11 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
+    
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def new
