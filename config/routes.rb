537ba030f9ab23c63e5a071e5aa54e5f25e7446e
TaitaT <taita.omori@gmail.com>
2020-08-22 12:33:20 +0900

    商品購入ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:new, :create, :show, :edit,:update,:destroy]
+  resources :items, only: [:new, :create, :show, :edit,:update,:destroy] do
+    resources :orders, only: [:index]
+  end
   get 'items/account', to: 'items#account'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_050526) do
+ActiveRecord::Schema.define(version: 2020_08_22_013945) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -37,15 +37,24 @@ ActiveRecord::Schema.define(version: 2020_08_19_050526) do
     t.bigint "user_id", null: false
     t.string "name", null: false
     t.text "description", null: false
-    t.integer "categories_id", null: false
-    t.integer "conditions_id", null: false
-    t.integer "postage_payers_id", null: false
+    t.integer "category_id"
+    t.integer "condition_id"
+    t.integer "postage_payer_id"
     t.integer "prefecture_id", null: false
-    t.integer "handling_times_id", null: false
+    t.integer "handling_time_id"
     t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -77,6 +86,8 @@ ActiveRecord::Schema.define(version: 2020_08_19_050526) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
   add_foreign_key "purchases", "items"
   add_foreign_key "purchases", "users"
 end
