ae8549b7137263ddaf64aa41f25ed10e293f3d94
TaitaT <taita.omori@gmail.com>
2020-08-25 11:50:12 +0900

    商品画像のバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,10 +10,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :postage_payer
   belongs_to_active_hash :prefecture
 
+  validate :image_presence
+
   validates :user, :name, :description, presence: true
 
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   validates :category_id, :condition_id, :handling_time_id,
             :postage_payer_id, :prefecture_id, presence: true, numericality: { other_than: 1 }
+
+  def image_presence
+    unless self.image.attached?
+      errors.add(:image, "can't be blank")
+    end
+  end
 end
